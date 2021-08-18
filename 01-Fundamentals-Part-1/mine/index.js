3c7aded98a6c749513e32dd1404bbe2866fe58da
Dane Laban <Siaki24@gmail.com>
2021-08-17 19:15:36 -0700

    update

---
+++ 01-Fundamentals-Part-1/mine/index.js
@@ -0,0 +1,124 @@
+// ----- SECTION 2 VIDEO 9 -----
+
+/*
+ let js = "amazing";
+ if (js === "amazing") alert ("Javascript is blended");
+
+ console.log(40 + 24 + 17);
+// --- END S2.V9 ---
+
+// ----- SECTION 2 VIDEO 10 -----
+ let firstName = "Kobe";
+ console.log(firstName); //Don't need quotes for variables. Variable is firstName.
+ */
+
+// --- END S2.V10 ---
+
+/* ---- Practice Assignment 1 ----
+
+const country = "USA";
+const continent = "North America";
+const isIsland = "false"; // FALSE and TRUE are BOOLEANS
+let population = 328200000;
+let language = "English";
+let description = "USA is in North America, and it's 328.2 million people speak English."
+
+console.log(country);
+console.log(continent);
+console.log(isIsland);
+console.log(population);
+console.log(language);
+console.log(description);
+*/
+
+/* 
+-------- SIDE NOTE ---------
+NUMBER: Numbers => Used for decimals and integers. ex| let age = 23;
+
+STRING: Sequence of characters => Text. ex| let firstName = "Kobe";
+
+BOOLEAN: Can ONLY be 'TRUE' or 'FALSE' => Taking decisions. ex| let fullAge = true;
+
+Undefined: Value is taken by a variable that is not defined aka empty value. ex| let children;
+
+Null: Also an empty value. ex| let kids;
+
+Symbol: Value is unique and cannot be changed.
+
+BigInt: Larger integers than the Number type can hold.
+*/
+
+// ----- SECTION 2 VIDEO 12 -------
+
+/*
+ let javascriptIsFun = true; // BOOLEAN
+ console.log(javascriptIsFun);
+
+ console.log(typeof true);            // OPERATOR, goes into console as "BOOLEAN".
+ console.log(typeof javascriptIsFun); // OPERATOR, goes into console as "BOOLEAN".
+ console.log(typeof 23);              // OPERATOR, goes into console as "NUMBER".
+ console.log(typeof 'Kobe');          // OPERATOR, goes into console as "STRING". STRINGS need to have quotations.
+*/
+
+// Called DYNAMIC TYPING
+// YEAR starts as a variable then we give YEAR a value
+/*
+let year;
+console.log(year);
+
+year = 1990;
+console.log(year);
+*/
+// --- END S2.V12 --- 
+
+// ----- SECTION 12 VIDEO 13 ------
+
+// let, const, var
+// const creates a variable that cannot be changed
+// VAR is similar to LET. Never use VAR
+// ------ END S12.V13 ------
+
+// ------ SECTION 14 VIDEO 14 --------
+// BASIC OPERATORS
+// OPERATOR allows to transorm values or combine values.
+// Mathematical OPERATORS
+
+// Mathematical OPERATOR
+/*
+const now = 2021;               // Created because its not good to use the same value for multiple CONST. 
+const ageKobe = now - 1978;     // using current year minus birth year to get age. Use NOW const to input that year instead of having same const multiple times. 
+const ageShaq = now - 1972;     // using current year minus birth year to get age. Use NOW const to input that year instead of having same const multiple times.
+console.log (ageKobe, ageShaq); // Log more than one console with a COMMA
+
+console.log (ageKobe * 2, ageKobe / 10, 2 ** 5);
+*/
+/*  ------ SIDE NOTE for MATH OPERATORS -------
+    * is MULTIPLY
+    / is DIVISION
+    + is ADDITION
+    - is SUBTRACTION
+    ** is power -- 2 ** 4 means 2 to the power of 4 or 2 * 2 * 2 * 2...
+*/
+/*
+const firstName = 'Kobe';
+const lastName = 'Bryant';
+console.log (firstName + lastName);         // shows as KobeBryant in console
+console.log (firstName + ' ' + lastName);   // shows as Kobe Braynt in console. ADDITION combines the strings.
+
+// Assignment OPERATORS
+let x = 10 + 5;     // this equals 15
+x += 10;            // this equals 25. Simpler way to write x = x + 15.
+x ++;               // this equals 26. This means x = x + 1 aka x = 25 + 1 -- This adds 1 to whatever x is.
+x --;               // this equals 25. This means x = x - 1 aka x = 26 - 1 -- This SUBTRACTS 1 to whatever x is, in this case 26 because of line 97.
+console.log (x);
+
+// Comparison OPERATORS aka BOOLEAN OPERATOR. Types of BOOLEAN COMPARISONS:: <, >, >=, <=
+console.log (ageKobe > ageShaq); // Gives a true or false statement in console... ANSWER is 'FALSE'
+console.log (ageShaq >= 27, ageShaq <= ageKobe); // If we wanted to know if something is greater/less than or equal to something else... ANSWER is 'TRUE FALSE'
+*/
+
+// ------- END S2.V14 -------
+
+// ------- SECTION 2 VIDEO 15 ---------
+
+// OPERATOR PROCEDURE
